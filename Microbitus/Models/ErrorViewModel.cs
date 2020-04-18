9e867689844e8c46d1b8c1f0089682812b0eccca
Saif Shakil Shaikh <me.shaikhsaif@outlook.com>
2020-04-18 07:04:58 +0530

    Add project files.

---
+++ Microbitus/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Microbitus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
